9cbb5ada9490a2fe459b387a84d9b008f4318aa0
zach6585 <zmines@wesleyan.edu>
2020-12-19 20:14:12 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  if (parseInt(time.split(":")[0]) < 12) {
+    return "Good Morning";
+  }
+
+  else if (12 <= parseInt(time.split(":")[0]) && parseInt(time.split(":")[0]) <= 17) {
+    return "Good Afternoon";
+  }
+
+  else if (parseInt(time.split(":")[0]) > 17) {
+    return "Good Evening";
+
+  
+  }
+  else {
+    return time;
+  }
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+    let header = document.getElementById("greeting");
+    header.innerHTML = message;
+}
